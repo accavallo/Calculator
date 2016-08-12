95eafdc08bf8b990e5c5e2641ef5abee5d1afc73
Tony <anthony.c.cavallo@gmail.com>
2016-08-12 15:41:55 -0500

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Tony on 8/12/16.
+//  Copyright © 2016 Anthony Cavallo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
